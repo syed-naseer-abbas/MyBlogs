b7784bf269062aa1c9c724c11788358dd5bba1d4
syed-naseer-abbas <66269007+syed-naseer-abbas@users.noreply.github.com>
2021-02-23 13:29:51 +0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+
+class IndexPage extends React.Component {
+  render() {
+    const siteTitle = "Gatsby Starter Personal Website"
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Home"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <h1>
+          Hey All{" "}
+          <span role="img" aria-label="wave emoji">
+            👋
+          </span>
+        </h1>
+        <p>Welcome to my website.</p>
+        <p>
+          We'll talk about Technology and share experiences and knowledge.
+        </p>
+        <p>Lets Start!</p>
+        <Link to="/blog/">
+          <Button marginTop="35px">Go to Blog</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
